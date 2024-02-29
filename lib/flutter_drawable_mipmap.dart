c05ec90818bb89dcf394ab0162c47f51f21b3b8d
Dhiraj Sharma <sharmadhiraj.np@gmail.com>
2024-02-29 10:28:39 +0545

    Add documentation to plugin dart code

--- lib/flutter_drawable_mipmap.dart
+++ lib/flutter_drawable_mipmap.dart
@@ -1,20 +1,22 @@
 import 'dart:async';
-import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 
+/// A Flutter plugin for accessing drawable and mipmap resources.
 class FlutterDrawableMipmap {
   static const MethodChannel _channel =
-      const MethodChannel('flutter_drawable_mipmap');
+      const MethodChannel("flutter_drawable_mipmap");
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-
+  /// Retrieves the byte array of the drawable or mipmap with the given [name].
+  ///
+  /// The [isDrawable] parameter specifies whether the resource is a drawable
+  /// (`true`) or a mipmap (`false`).
+  ///
+  /// Returns a [Uint8List] containing the byte array of the drawable or mipmap,
+  /// or `null` if the resource is not found.
   static Future<Uint8List?> drawableMipmap(String name, bool isDrawable) async {
     return await _channel.invokeMethod(
-      'drawableMipmap',
+      "drawableMipmap",
       {
         "name": name,
         "is_drawable": isDrawable,
--- lib/image_widget.dart
+++ lib/image_widget.dart
@@ -4,18 +4,25 @@ import 'package:flutter/material.dart';
 
 import 'flutter_drawable_mipmap.dart';
 
+/// An abstract widget for displaying drawable or mipmap images dynamically loaded
+/// from resources.
 abstract class _ImageWidget extends StatefulWidget {
+  /// The name of the drawable or mipmap resource to load.
   final String name;
 
+  /// Indicates whether the resource is a drawable (`true`) or a mipmap (`false`).
   bool isDrawable();
 
+  /// Creates a new instance of [_ImageWidget] with the given [name].
   _ImageWidget(this.name);
 
   @override
   __ImageWidgetState createState() => __ImageWidgetState();
 }
 
+/// The state associated with the [_ImageWidget].
 class __ImageWidgetState extends State<_ImageWidget> {
+  /// The byte array representation of the loaded image resource.
   Uint8List? image;
 
   @override
@@ -24,6 +31,7 @@ class __ImageWidgetState extends State<_ImageWidget> {
     getDrawableMipmap();
   }
 
+  /// Loads the drawable or mipmap resource asynchronously.
   Future<void> getDrawableMipmap() async {
     FlutterDrawableMipmap.drawableMipmap(
       widget.name,
@@ -39,14 +47,18 @@ class __ImageWidgetState extends State<_ImageWidget> {
   }
 }
 
+/// A widget for displaying drawable images loaded dynamically from resources.
 class Drawable extends _ImageWidget {
+  /// Creates a new instance of [Drawable] with the given [name].
   Drawable(String name) : super(name);
 
   @override
   bool isDrawable() => true;
 }
 
+/// A widget for displaying mipmap images loaded dynamically from resources.
 class Mipmap extends _ImageWidget {
+  /// Creates a new instance of [Mipmap] with the given [name].
   Mipmap(String name) : super(name);
 
   @override
