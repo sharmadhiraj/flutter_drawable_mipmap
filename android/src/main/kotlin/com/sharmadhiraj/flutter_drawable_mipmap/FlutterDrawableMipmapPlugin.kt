92f3767968b57f374deccec8f1192867fb26685f
Ridwan Fauzan <rymetutor@gmail.com>
2024-07-03 15:02:49 +0700

    Update FlutterDrawableMipmapPlugin.kt

--- android/src/main/kotlin/com/sharmadhiraj/flutter_drawable_mipmap/FlutterDrawableMipmapPlugin.kt
+++ android/src/main/kotlin/com/sharmadhiraj/flutter_drawable_mipmap/FlutterDrawableMipmapPlugin.kt
@@ -3,6 +3,7 @@ package com.sharmadhiraj.flutter_drawable_mipmap
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Bitmap.CompressFormat
+import android.graphics.drawable.AdaptiveIconDrawable
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.util.Log
@@ -39,9 +40,13 @@ class FlutterDrawableMipmapPlugin : FlutterPlugin, MethodCallHandler {
                 if (isDrawable) "drawable" else "mipmap",
                 context?.packageName
             )
-            val drawable: Drawable? = ContextCompat.getDrawable(context!!, id!!)
-            val byteArray = drawableToByteArray(drawable)
-            result.success(byteArray)
+            if (id != null && id != 0) {
+                val drawable: Drawable? = ContextCompat.getDrawable(context!!, id)
+                val byteArray = drawableToByteArray(drawable)
+                result.success(byteArray)
+            } else {
+                result.error("NOT_FOUND", "Resource not found", null)
+            }
         } else {
             result.notImplemented()
         }
@@ -56,10 +61,24 @@ class FlutterDrawableMipmapPlugin : FlutterPlugin, MethodCallHandler {
             Log.e("FlutterDrawableMipmap", "Drawable is null")
             return ByteArray(0)
         }
-        val bitmap: Bitmap = (drawable as BitmapDrawable).bitmap
+
+        val bitmap: Bitmap = when (drawable) {
+            is BitmapDrawable -> drawable.bitmap
+            is AdaptiveIconDrawable -> getBitmapFromAdaptiveIcon(drawable)
+            else -> throw IllegalArgumentException("Unsupported drawable type")
+        }
+
         val stream = ByteArrayOutputStream()
-        bitmap.compress(CompressFormat.PNG, 100, stream)
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
         return stream.toByteArray()
     }
 
+    private fun getBitmapFromAdaptiveIcon(drawable: AdaptiveIconDrawable): Bitmap {
+        val iconSize = 108 // Adaptive icons are typically 108dp in size
+        val bitmap = Bitmap.createBitmap(iconSize, iconSize, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+        drawable.draw(canvas)
+        return bitmap
+    }
 }
